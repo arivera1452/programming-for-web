39e399c75f873a21d82afc27bf230334bf1b2862
arivera1452 <arivera@mcad.edu>
2025-02-27 19:06:39 -0600

    6.2

---
+++ week6/6.2/src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+  const name = "Andrew";
+  const color = "purple";
+  return (
+    <div>
+      <h1>Hello World</h1>
+      <p>My name is {name} and my favorite color is {color}.</p>
+    </div>
+  )
+}
+
+export default App
