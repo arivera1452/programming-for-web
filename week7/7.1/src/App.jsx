ea63c826693c2a41c5f514c63ae7c9b4533d6bb9
arivera1452 <arivera@mcad.edu>
2025-03-09 14:22:32 -0500

    7.1 fix again

--- week7/7.1/src/App.jsx
+++ week7/7.1/src/App.jsx
@@ -1,16 +1,14 @@
 import Camelid from './components/Camelid.jsx';
-import Llama from './assets/llama.jpg';
-import Alpaca from './assets/alpaca.jpg'
 
 function App() {
   const llamas = {
     name: 'Llama',
-    image: Llama,
+    image: "./llama.jpg",
     fact: 'A llama can carry as much as 200 pounds for 12 hours a day, but are not ridden.'
   };
   const alpacas = {
     name: 'Alpaca',
-    image: Alpaca,
+    image: "./alpaca.jpg",
     fact: 'Alpacas are highly sociable, gentle and curious.'
   };
 
