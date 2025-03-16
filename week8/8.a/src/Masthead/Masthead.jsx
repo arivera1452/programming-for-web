e4efb140466826bb680f3a3f2b4becf3a8eab0c6
arivera1452 <arivera@mcad.edu>
2025-03-16 17:56:26 -0500

    8.a

---
+++ week8/8.a/src/Masthead/Masthead.jsx
@@ -0,0 +1,10 @@
+import mastheadImg from "../assets/masthead.jpg";
+import "./Masthead.css"
+export default function Masthead() {
+    return(
+        <div className="masthead">
+            <img src={mastheadImg} alt="Andrew's Commander Decks" />
+            <h1>Andrew&apos;s Commander Decks</h1>
+        </div>
+    )
+}
